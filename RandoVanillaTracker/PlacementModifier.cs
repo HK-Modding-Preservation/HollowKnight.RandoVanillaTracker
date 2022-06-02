537d178c2ed72b5ba6f862a6836876f3be237efb
syyePhenomenol <yye3028@gmail.com>
2022-06-02 11:41:59 +1000

    Partial fix to shop rebalancing

--- RandoVanillaTracker/PlacementModifier.cs
+++ RandoVanillaTracker/PlacementModifier.cs
@@ -1,4 +1,6 @@
-﻿using RandomizerMod.Logging;
+﻿using ItemChanger;
+using RandomizerCore.Extensions;
+using RandomizerMod.Logging;
 using RandomizerMod.RandomizerData;
 using RandomizerMod.RC;
 using RandomizerMod.Settings;
@@ -24,6 +26,8 @@ namespace RandoVanillaTracker
             RequestBuilder.OnUpdate.Subscribe(5000f, TrackInteropItems);
             RequestBuilder.OnUpdate.Subscribe(300f, DerandomizeTrackedItems);
 
+            RequestBuilder.OnUpdate.Subscribe(1.5f, RebalanceShopCounts);
+
             SettingsLog.AfterLogSettings += LogRVTSettings;
         }
 
@@ -48,7 +52,8 @@ namespace RandoVanillaTracker
             {
                 if (Data.IsTransition(vd.Item) && Data.IsTransition(vd.Location))
                 {
-                    rb.RemoveFromVanilla(vd);
+                    //rb.RemoveFromVanilla(vd);
+                    RemoveFromVanilla(rb, vd);
                     vb.VanillaTransitions.Add(vd);
                 }
             }
@@ -67,7 +72,8 @@ namespace RandoVanillaTracker
                         if (rb.Vanilla.TryGetValue(vd.Location, out List<VanillaDef> defs))
                         {
                             int count = defs.Count(x => x == vd);
-                            rb.RemoveFromVanilla(vd);
+                            //rb.RemoveFromVanilla(vd);
+                            RemoveFromVanilla(rb, vd);
 
                             for (int i = 0; i < count; i++)
                             {
@@ -83,6 +89,7 @@ namespace RandoVanillaTracker
         private static void RecordTrackedPools(RequestBuilder rb)
         {
             _recordedPools.Clear();
+            _shopVanillaCounts.Clear();
 
             // Add a group that will catch all vanilla items
             StageBuilder sb = rb.InsertStage(0, "RVT Item Stage");
@@ -103,6 +110,19 @@ namespace RandoVanillaTracker
                     foreach (VanillaDef vd in pool.Vanilla)
                     {
                         vb.VanillaPlacements.Add(vd);
+
+                        // Record the number of vanilla items that each shop will hold, to fix rebalancing issues later
+                        if (ShopNames.Contains(vd.Location))
+                        {
+                            if (_shopVanillaCounts.ContainsKey(vd.Location))
+                            {
+                                _shopVanillaCounts[vd.Location]++;
+                            }
+                            else
+                            {
+                                _shopVanillaCounts.Add(vd.Location, 1);
+                            }
+                        }
                     }
                 }
             }
@@ -137,11 +157,93 @@ namespace RandoVanillaTracker
                     //rb.RemoveFromVanilla(vd);
 
                     // Temporary fixed version of RemoveFromVanilla
-                    if (rb.Vanilla.TryGetValue(vd.Location, out List<VanillaDef> defs))
+                    RemoveFromVanilla(rb, vd);
+                }
+            }
+        }
+
+        private static void RemoveFromVanilla(RequestBuilder rb, VanillaDef vd)
+        {
+            if (rb.Vanilla.TryGetValue(vd.Location, out List<VanillaDef> defs))
+            {
+                defs.RemoveAll(def => def.Equals(vd));
+            }
+        }
+
+        private static Dictionary<string, int> _shopVanillaCounts = new();
+
+        public static readonly HashSet<string> ShopNames = new()
+        {
+            LocationNames.Iselda,
+            LocationNames.Sly,
+            LocationNames.Sly_Key,
+            LocationNames.Salubra,
+            LocationNames.Leg_Eater,
+            LocationNames.Seer,
+            LocationNames.Grubfather
+        };
+
+        // Rebalance so there are at least enough locations in each shop for vanilla items
+        private static void RebalanceShopCounts(RequestBuilder rb)
+        {
+            Dictionary<string, HashSet<string>> multiSets = new();
+
+            Dictionary<string, int> counts = new();
+
+            // Decouple the counting from the rebalancing
+            foreach (ItemGroupBuilder gb in rb.EnumerateItemGroups())
+            {
+
+                multiSets.Add(gb.label, new());
+
+                int count = 0;
+                foreach (string l in gb.Locations.EnumerateDistinct())
+                {
+                    if (rb.TryGetLocationDef(l, out LocationDef def) && def.FlexibleCount)
                     {
-                        defs.RemoveAll(def => def.Equals(vd));
+                        multiSets[gb.label].Add(l);
+                        count += gb.Locations.GetCount(l);
                     }
                 }
+
+                counts.Add(gb.label, count);
+            }
+
+            // Allocate vanilla slots by removing them evenly from counts
+            int vanillaTotalCount = _shopVanillaCounts.Values.Sum();
+
+            int countPerGB = Math.DivRem(vanillaTotalCount, counts.Count(), out int rem);
+
+            foreach(ItemGroupBuilder gb in rb.EnumerateItemGroups())
+            {
+                counts[gb.label] -= countPerGB;
+
+                // Also distribute remainder evenly
+                if (rem > 0)
+                {
+                    counts[gb.label]--;
+                    rem--;
+                }
+            }
+
+            //TODO: Make sure each group gets one of each location if possible
+
+            foreach (ItemGroupBuilder gb in rb.EnumerateItemGroups())
+            {
+                string[] multi = multiSets[gb.label].OrderBy(s => s).ToArray();
+                foreach (string l in multi)
+                {
+                    gb.Locations.Set(l, 0);
+                }
+                while (counts[gb.label]-- > 0)
+                {
+                    gb.Locations.Add(rb.rng.Next(multi));
+                }
+            }
+
+            foreach (KeyValuePair<string, int> kvp in _shopVanillaCounts)
+            {
+                ((ItemGroupBuilder)rb.GetGroupFor(kvp.Key, ElementType.Location)).Locations.AddRange(Enumerable.Repeat(kvp.Key, kvp.Value));
             }
         }
     }
